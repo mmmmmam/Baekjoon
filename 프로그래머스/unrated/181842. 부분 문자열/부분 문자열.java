2911048f71ec473d766b8c8335faf7e8414a61c1
mmmmmam <144782356+mmmmmam@users.noreply.github.com>
2023-10-19 15:58:22 +0900

    [level unrated] Title: 부분 문자열, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String str1, String str2) {
+        int answer = 0;
+        
+        boolean a = str2.contains(str1);
+        
+        if(a == true) answer = 1;
+        
+        return answer;
+    }
+}
