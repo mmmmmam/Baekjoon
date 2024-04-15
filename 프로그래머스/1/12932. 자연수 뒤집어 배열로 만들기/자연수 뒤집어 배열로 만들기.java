6ef3413918fe92b165f56acef65ace76a23c211f
마음 <144782356+mmmmmam@users.noreply.github.com>
2024-04-15 16:27:03 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.17 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(long n) {
+        String s = String.valueOf(n);
+        int[] answer = new int[s.length()];
+        
+        for(int i = 0; i<answer.length; i++) {
+        	answer[i] = Integer.parseInt(s.substring(answer.length-1-i, answer.length-i));
+        }
+        
+        System.out.println(Arrays.toString(answer));
+        return answer;
+    }
+}
