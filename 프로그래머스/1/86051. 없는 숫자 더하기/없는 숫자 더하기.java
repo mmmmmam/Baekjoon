302fdc82fa073ada5c364e564d597c9c2c2766ae
마음 <144782356+mmmmmam@users.noreply.github.com>
2024-08-01 17:31:02 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,21 +1,23 @@
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-		int[] arr = new int[10];
-		
-		//Arrays.sort(numbers);
-		
-		for(int i = 0; i<numbers.length; i++) {
-			arr[numbers[i]] = 1;
-		}
-		
-		for(int j = 0; j<arr.length; j++) {
-			if(arr[j] == 0) {
-				answer += j;
-			}
-		}
-		
-		//System.out.println(Arrays.toString(arr));
-        return answer;
+        // 0부터 9까지의 숫자가 존재하는지 여부를 기록하는 배열
+        boolean[] seen = new boolean[10];
+        
+        // 주어진 배열을 순회하면서 존재하는 숫자를 기록
+        for (int number : numbers) {
+            if (number >= 0 && number <= 9) {
+                seen[number] = true;
+            }
+        }
+        
+        // 존재하지 않는 숫자를 찾아서 합산
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            if (!seen[i]) {
+                sum += i;
+            }
+        }
+        
+        return sum;
     }
 }
