e20db6b3603c3bde9b7908d94415e3a23c9893c2
마음 <144782356+mmmmmam@users.noreply.github.com>
2024-08-19 09:50:41 +0900

    [level 2] Title: 방문 길이, Time: 23.96 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -0,0 +1,56 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int solution(String dirs) {
+        // 이동 방향을 정의합니다.
+        int[][] directions = {
+            {0, 1},   // U: 위쪽
+            {0, -1},  // D: 아래쪽
+            {1, 0},   // R: 오른쪽
+            {-1, 0}   // L: 왼쪽
+        };
+
+        // 방문한 길을 추적하기 위한 Set
+        Set<String> visited = new HashSet<>();
+
+        // 현재 위치 초기화
+        int x = 0, y = 0;
+        int length = 0;
+
+        // 명령어를 하나씩 처리합니다.
+        for (char command : dirs.toCharArray()) {
+            int dir = 0;
+            switch (command) {
+                case 'U': dir = 0; break;
+                case 'D': dir = 1; break;
+                case 'R': dir = 2; break;
+                case 'L': dir = 3; break;
+            }
+
+            // 다음 위치 계산
+            int newX = x + directions[dir][0];
+            int newY = y + directions[dir][1];
+
+            // 경계 체크
+            if (newX < -5 || newX > 5 || newY < -5 || newY > 5) {
+                continue;
+            }
+
+            // 길을 문자열로 표현하여 방문 체크
+            String path1 = x + "," + y + ":" + newX + "," + newY;
+            String path2 = newX + "," + newY + ":" + x + "," + y;
+
+            if (!visited.contains(path1) && !visited.contains(path2)) {
+                length++;
+                visited.add(path1);
+            }
+
+            // 현재 위치 갱신
+            x = newX;
+            y = newY;
+        }
+
+        return length;
+    }
+}
